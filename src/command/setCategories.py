5d2ef6c28caa6c2c6ca4dbb7d9c9cb7fb6d1edaa
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-05-03 13:53:04 +0200

    chore: Update setCategories.py to support multiple languages and versions

--- src/command/setCategories.py
+++ src/command/setCategories.py
@@ -4,7 +4,7 @@ sys.path.append("..")
 
 from service.discover import getCategoryTree
 
-def setCategories(category, parentCategory = None, akeneoCategories = {}):
+def setCategories(category, parentCategory = None, akeneoCategories = {}, language = 'en_US'):
     # check if category is a list
     if isinstance(category, list):
         for cat in category:
@@ -17,9 +17,9 @@ def setCategories(category, parentCategory = None, akeneoCategories = {}):
             akeneoCategories[cat['identifier']]["parent"] = parentCategory
             if 'labels' not in akeneoCategories[cat['identifier']]:
                 akeneoCategories[cat['identifier']]["labels"] = {}
-            akeneoCategories[cat['identifier']]["labels"]["en_US"] = cat['name']
+            akeneoCategories[cat['identifier']]["labels"][language] = cat['name']
             if 'children' in cat:
-                setCategories(cat['children'], cat['identifier'], akeneoCategories)
+                setCategories(cat['children'], cat['identifier'], akeneoCategories, language)
     else:
         print("Category: ")
         print(category['identifier'])
@@ -30,9 +30,18 @@ def setCategories(category, parentCategory = None, akeneoCategories = {}):
         akeneoCategories[category['identifier']]["parent"] = parentCategory
         if 'labels' not in akeneoCategories[category['identifier']]:
             akeneoCategories[category['identifier']]["labels"] = {}
-        akeneoCategories[category['identifier']]["labels"]["en_US"] = category['name']
+        akeneoCategories[category['identifier']]["labels"][language] = category['name']
         if 'children' in category:
-            setCategories(category['children'], category['identifier'], akeneoCategories)
+            setCategories(category['children'], category['identifier'], akeneoCategories, language)
+
+    return akeneoCategories
+
+# set Label for each language
+def addLabeltoCategory(category, language, akeneoCategories):
+    for key in akeneoCategories:
+        print("Category: ")
+        print(category)
+        akeneoCategories[key]["labels"][language] = category[key]
 
     return akeneoCategories
 
@@ -43,8 +52,11 @@ def main():
     categoriesDE = getCategoryTree(category, 'de')
     categoriesFR = getCategoryTree(category, 'fr')
     categoriesIT = getCategoryTree(category, 'it')
-    
+
     akeneoCategories = setCategories(categoriesEN)
+    akeneoCategories = setCategories(categoriesDE, None, akeneoCategories, 'de_CH')
+    akeneoCategories = setCategories(categoriesFR, None, akeneoCategories, 'fr_FR')
+    akeneoCategories = setCategories(categoriesIT, None, akeneoCategories, 'it_IT')
 
     with open("../../output/akeneoCategories.json", "w") as file:
         json.dump(akeneoCategories, file)
